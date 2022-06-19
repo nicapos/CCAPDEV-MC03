e111caea317bf722f5686c5f38f639e283f38073
nicapos <annikaposadas22@gmail.com>
2022-06-19 18:21:02 +0800

    Complete the TransactionSchema

--- ccapdev-2021t2-mc03-main/models/TransactionModel.js
+++ ccapdev-2021t2-mc03-main/models/TransactionModel.js
@@ -1,13 +1,10 @@
 
 var mongoose = require('mongoose');
 
-/*
-    TODO:   Complete the TransactionSchema which will contain the name,
-            reference number, and the amount of a transaction in the database.
-*/
-
 var TransactionSchema = new mongoose.Schema({
-    // your code here
+    name: String,
+    refno: Number,
+    amount: Number
 });
 
 module.exports = mongoose.model('Transaction', TransactionSchema);
